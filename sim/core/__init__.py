da1aaa868800af3a9c0576723f25ca1a6fd4ca24
TimeWz667 <TimeWz667@gmail.com>
2023-03-13 15:40:25 +0000

    Delete legacy models

--- sim/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from sim.core.inputs import load_inputs
-from sim.core.cascade import *
-from sim.core.dy import Model
-from sim.core.obj import Objective
--- sim/ind/__init__.py
+++
@@ -1,2 +0,0 @@
-from sim.ind.dy import ModelIND, get_intv
-from sim.ind.dpupr import ModelPupr
